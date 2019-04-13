e5c3cd2b1605b959f09bf20b3be8f46a9df3c8ca
blue_sky <910790802@qq.com>
2019-04-13 21:38:31 +0800

    demo

--- src/main/java/com/demo/example/demo/Demo.java
+++ src/main/java/com/demo/example/demo/Demo.java
@@ -2,6 +2,6 @@ package com.demo.example.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hello boot");
+        System.out.println("hello boot!");
     }
 }
